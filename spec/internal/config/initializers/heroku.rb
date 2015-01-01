2d95630483f39bf88073a69a5c7d360c667b6da3
Pat Allan <pat@freelancing-gods.com>
2015-01-01 15:30:12 +1100

    Switch to a modern manifest example.

--- spec/internal/config/initializers/heroku.rb
+++ spec/internal/config/initializers/heroku.rb
@@ -2,7 +2,7 @@ require 'maitre_d/heroku'
 
 MaitreD::Heroku.configure do |config|
   config.id       = 'foo'
-  config.password = 'bar'
-  config.sso_salt = 'sea salt'
+  config.password = '056e8ff3d55ea6e1a7cc4aa7afeaf3ac'
+  config.sso_salt = 'db0ce73d4bd2dfa1d66c713a9aaf8f76'
   config.listener = HerokuListener
 end
