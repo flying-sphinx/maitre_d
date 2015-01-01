e3f45cb921360d9382cc8bf2b6a0e8afad9ce3c1
Pat Allan <pat@freelancing-gods.com>
2015-01-01 13:42:21 +1100

    Removing Opperator, doubtful it will ever exist.

--- spec/internal/config/initializers/opperator.rb
+++
@@ -1,6 +0,0 @@
-require 'maitre_d/opperator'
-
-MaitreD::Opperator.configure do |config|
-  config.shared_secret = 'something-special'
-  config.listener      = OpperatorListener
-end
--- spec/internal/config/routes.rb
+++ spec/internal/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   mount MaitreD::Heroku::API       => '/heroku'
   mount MaitreD::CloudControl::API => '/cloudcontrol'
-  mount MaitreD::Opperator::API    => '/opperator'
 
   class Dashboard
     def self.call(env)
